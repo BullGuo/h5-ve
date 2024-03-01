ea76896c8f615bea2a6521fe591a16cddd1c9123
jiangguo <1521323018@qq.com>
2024-03-01 22:19:51 +0800

    feat: 初步完成Text组件

---
+++ src/components/Text.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+import type { textDefaultType } from '~/types'
+import { textDefaultProps } from '~/defaultProps'
+
+const props = withDefaults(defineProps<textDefaultType>(), {
+  tag: 'div',
+  ...textDefaultProps,
+})
+</script>
+
+<template>
+  <component :is="tag" :style="props">
+    {{ text }}
+  </component>
+</template>
+
+<style scope>
+
+</style>
