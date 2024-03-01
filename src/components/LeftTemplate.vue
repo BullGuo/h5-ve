a7a281ec86cdf71452532d6ba5b109b87f5b8a3e
jiangguo <1521323018@qq.com>
2024-03-01 22:22:47 +0800

    feat: 初步完成左侧Template组件

---
+++ src/components/LeftTemplate.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+import type { textDefaultType } from '~/types'
+
+defineProps<{
+  list: Array<textDefaultType>
+}>()
+
+const emits = defineEmits<{ onTemplateClick: [data: textDefaultType] }>()
+
+function handleItemClick(data: textDefaultType) {
+  emits('onTemplateClick', data)
+}
+</script>
+
+<template>
+  <div v-for="(item, index) of list" :key="index" @click="handleItemClick(item)">
+    <Text v-bind="item" />
+  </div>
+</template>
