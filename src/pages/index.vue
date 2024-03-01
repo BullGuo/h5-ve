92a1a83324d046fa6f7349908937951619d089c3
jiangguo <1521323018@qq.com>
2024-03-01 22:27:06 +0800

    feat: 整合初步逻辑

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,30 @@
 <script setup lang="ts" generic="T extends any, O extends any">
+import { v4 as uuidv4 } from 'uuid'
+import type { ComponentData, DynamicComponent, textDefaultType } from '~/types'
+import Text from '~/components/Text.vue'
+import { defaultTextTemplates } from '~/defaultTemplate'
+
+const components: DynamicComponent = { Text }
+
+const formComponents: ComponentData<textDefaultType>[] = reactive([
+  { id: uuidv4(), name: 'Text', props: { fontSize: '32px', tag: 'h1', text: '123', color: 'red' } },
+  { id: uuidv4(), name: 'Text', props: { fontSize: '16px', tag: 'h3', text: '456', color: 'black' } },
+])
+
+function handleTemplateClick(data: textDefaultType) {
+  const newData = {
+    id: uuidv4(),
+    name: 'Text',
+    props: data,
+  }
+  formComponents.push(newData)
+}
+
+const currentElement = ref<ComponentData<textDefaultType>>()
+
+function handleEditClick(id: string) {
+  currentElement.value = formComponents.find(item => item.id === id)
+}
 </script>
 
 <template>
@@ -6,12 +32,24 @@
     <el-container h-full>
       <el-aside w="1/4!" bg-green>
         Aside
+        <left-template :list="defaultTextTemplates" @on-template-click="handleTemplateClick" />
       </el-aside>
       <el-main bg-yellow>
         Main
+        <edit-wrapper
+          v-for="component of formComponents"
+          :id="component.id"
+          :key="component.id"
+          :is-active="currentElement?.id === component.id"
+          @on-edit-click="handleEditClick"
+        >
+          <pre>{{ component.id }}</pre>
+          <component :is="components[component.name]" v-bind="component.props" />
+        </edit-wrapper>
       </el-main>
       <el-aside w="1/4!" bg-red>
         Aside
+        <pre>{{ currentElement?.props }}</pre>
       </el-aside>
     </el-container>
   </div>
