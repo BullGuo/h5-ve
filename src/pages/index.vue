7917f72d91d9ccbcc054e2a336d2c439ba31f07b
jiangguo <1521323018@qq.com>
2024-03-01 21:28:27 +0800

    feat: 添加基本布局

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,18 @@
 <script setup lang="ts" generic="T extends any, O extends any">
-import { storeToRefs } from 'pinia'
-import { useCounterStore } from '~/store'
-
-const store = useCounterStore()
-const { count } = storeToRefs(store)
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-    <el-button type="danger">
-      sadsadsa
-    </el-button>
-    <div p-10>
-      {{ count }}
-    </div>
+  <div h-full>
+    <el-container h-full>
+      <el-aside w="1/4!" bg-green>
+        Aside
+      </el-aside>
+      <el-main bg-yellow>
+        Main
+      </el-main>
+      <el-aside w="1/4!" bg-red>
+        Aside
+      </el-aside>
+    </el-container>
   </div>
 </template>
