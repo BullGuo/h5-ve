80872a12ea774b1b97b49035fb5dbf74b1a6dedc
jiangguo <1521323018@qq.com>
2024-03-04 20:05:19 +0800

    feat: 将数据提取到store中

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,48 +1,22 @@
 <script setup lang="ts" generic="T extends any, O extends any">
-import { v4 as uuidv4 } from 'uuid'
-import type { ComponentData, DynamicComponent, textDefaultType } from '~/types'
-import Text from '~/components/Text.vue'
+import { storeToRefs } from 'pinia'
+import type { DynamicComponent } from '~/types'
 import { defaultTextTemplates } from '~/defaultTemplate'
+import { useEditorStore } from '~/store'
+import Text from '~/components/Text.vue'
 
 const components: DynamicComponent = { Text }
 
-const formComponents = ref<ComponentData<textDefaultType>[]>([
-  { id: uuidv4(), name: 'Text', props: { fontSize: '32px', tag: 'h1', text: '123', color: '#1E90FF', textAlign: 'left', fontFamily: '"SimSun","STSong"', width: '100px', height: '50px', backgroundColor: '' } },
-  { id: uuidv4(), name: 'Text', props: { fontSize: '16px', tag: 'h3', text: '456', color: '#49118D', lineHeight: 2, textAlign: 'center', fontFamily: '', backgroundColor: '#28C913', fontWeight: 'bold', fontStyle: 'italic', textDecoration: '' } },
-])
-
-function handleTemplateClick(data: textDefaultType) {
-  const newData = {
-    id: uuidv4(),
-    name: 'Text',
-    props: data,
-  }
-  formComponents.value.push(newData)
-}
-
-const currentElement = ref<ComponentData<textDefaultType>>()
-
-function handleEditClick(id: string) {
-  currentElement.value = formComponents.value.find(item => item.id === id)
-}
-
-function handleEditDel(id: string) {
-  formComponents.value = formComponents.value.filter(item => item.id !== id)
-  if (currentElement?.value)
-    currentElement.value = undefined
-}
-
-function handleChange({ key, value }: { key: string, value: any }) {
-  if (currentElement?.value)
-    currentElement.value.props[key as keyof textDefaultType] = value
-}
+const editorStore = useEditorStore()
+const { formComponents, currentElement } = storeToRefs(editorStore)
+const { addEditor, removeEditor, selectEditor, modifyEditor } = editorStore
 </script>
 
 <template>
   <div h-full>
     <el-container h-full>
       <el-aside w="1/4!" bg-green>
-        <left-template :list="defaultTextTemplates" @on-template-click="handleTemplateClick" />
+        <left-template :list="defaultTextTemplates" @on-template-click="addEditor" />
       </el-aside>
       <el-main bg-yellow>
         <edit-wrapper
@@ -50,14 +24,14 @@ function handleChange({ key, value }: { key: string, value: any }) {
           :id="component.id"
           :key="component.id"
           :is-active="currentElement?.id === component.id"
-          @on-edit-click="handleEditClick"
-          @on-edit-del="handleEditDel"
+          @on-edit-click="selectEditor"
+          @on-edit-del="removeEditor"
         >
           <component :is="components[component.name]" v-bind="component.props" />
         </edit-wrapper>
       </el-main>
-      <el-aside w="1/4!" bg-red>
-        <props-form v-if="currentElement" :form-value="currentElement?.props" @on-change="handleChange" />
+      <el-aside w="1/4!" bg-white>
+        <props-form v-if="currentElement" :form-value="currentElement?.props" @on-change="modifyEditor" />
       </el-aside>
     </el-container>
   </div>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,38 @@
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
+import type { ComponentData, textDefaultType } from '~/types'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useEditorStore = defineStore('counter', () => {
+  const formComponents = ref<ComponentData<textDefaultType>[]>([
+    { id: uuidv4(), name: 'Text', props: { fontSize: '32px', tag: 'h1', text: '123', color: '#1E90FF', textAlign: 'left', fontFamily: '"SimSun","STSong"', width: '100px', height: '50px', backgroundColor: '' } },
+    { id: uuidv4(), name: 'Text', props: { fontSize: '16px', tag: 'h3', text: '456', color: '#49118D', lineHeight: 2, textAlign: 'center', fontFamily: '', backgroundColor: '#28C913', fontWeight: 'bold', fontStyle: 'italic', textDecoration: '' } },
+  ])
+  const currentElement = ref<ComponentData<textDefaultType>>()
+
+  function addEditor(data: textDefaultType) {
+    const newData = {
+      id: uuidv4(),
+      name: 'Text',
+      props: data,
+    }
+    formComponents.value.push(newData)
   }
-  return { count, doubleCount, increment }
+
+  function removeEditor(id: string) {
+    formComponents.value = formComponents.value.filter(item => item.id !== id)
+    if (currentElement?.value)
+      currentElement.value = undefined
+  }
+
+  function selectEditor(id: string) {
+    currentElement.value = formComponents.value.find(item => item.id === id)
+  }
+
+  function modifyEditor({ key, value }: { key: string, value: any }) {
+    if (currentElement?.value)
+      currentElement.value.props[key as keyof textDefaultType] = value
+  }
+
+  return { formComponents, currentElement, addEditor, removeEditor, selectEditor, modifyEditor }
 })
